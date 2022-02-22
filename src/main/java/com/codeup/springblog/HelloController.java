54113f026a9417d723c7138733f87f7cb9a03f22
James Wienecke <james.a.wienecke@gmail.com>
2022-02-22 12:03:27 -0600

    Path variables! Awesome.

--- src/main/java/com/codeup/springblog/HelloController.java
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -2,13 +2,14 @@ package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/hello/{name}")
     @ResponseBody
-    public String hello() {
-        return "<h1>Hello from Spring!</h1>";
+    public String hello(@PathVariable String name) {
+        return "<h1>Hello " + name + "!</h1>";
     }
 }
