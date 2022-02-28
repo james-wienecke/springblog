e99651e830b0d9fc797036a560cb229326861c92
James Wienecke <james.a.wienecke@gmail.com>
2022-02-28 09:04:53 -0600

    Finished exercises.

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,43 +1,46 @@
 package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Post;
+import com.codeup.springblog.repositories.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @Controller
 public class PostController {
+    private PostRepository postRepo;
+
+    public PostController(PostRepository postRepo) {
+        this.postRepo = postRepo;
+    }
 
     @GetMapping("/posts")
     public String postsIndex(Model model) {
-        ArrayList<Post> posts = new ArrayList<>();
-        posts.add(new Post("Post 1", "A post like you've never seen before"));
-        posts.add(new Post("Post 2", "A post that you have seen before..."));
-        model.addAttribute("posts", posts);
+        model.addAttribute("posts", postRepo.findAll());
         return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
     public String postsIndividualPage(@PathVariable long id, Model model) {
-        Post post = new Post("This Post", "Test post for individual post view.");
-        model.addAttribute("post", post);
+        model.addAttribute("post", postRepo.findById(id));
         return "posts/show";
     }
 
     @GetMapping("/posts/create")
-    @ResponseBody
     public String postsCreateForm() {
-        return "view the form for creating a post";
+        return "posts/create";
     }
 
     @PostMapping("/posts/create")
     @ResponseBody
-    public String postsCreateSubmit() {
-        return "create a new post";
+    public String postsCreateSubmit(
+            @RequestParam(name = "title") String title,
+            @RequestParam(name = "body") String body
+    ) {
+        Post post = new Post(title, body);
+        postRepo.save(post);
+        return "Post created";
     }
 }
